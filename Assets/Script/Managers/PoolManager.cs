89d5fa200526fbb4204753be61b55a4331862467
februarywind <89524114+februarywind@users.noreply.github.com>
2024-09-26 15:17:34 +0900

    [feat] Manager And Monster

---
+++ Assets/Script/Managers/PoolManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] prefabs;
+    [SerializeField] Transform[] spwanPoint;
+    List<GameObject> pools = new List<GameObject> {};
+    Coroutine Coroutine;
+    private void Start()
+    {
+        Coroutine = StartCoroutine(SpwanStart());
+    }
+    public void CreateObj(int index)
+    {
+        foreach (var item in pools)
+        {
+            if(!item.activeSelf)
+            {
+                item.gameObject.transform.position = spwanPoint[Random.Range(0, spwanPoint.Length)].position;
+                item.gameObject.SetActive(true);
+                Debug.Log(item.gameObject.name);
+                return;
+            }
+        }
+        pools.Add(Instantiate(prefabs[index], spwanPoint[Random.Range(0, spwanPoint.Length)].position, transform.rotation, transform));
+    }
+    IEnumerator SpwanStart()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            CreateObj(0);
+        }
+    }
+}
---
+++ Assets/Script/Monster/MonsterState.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MonsterState : MonoBehaviour
+{
+    [SerializeField] float maxhp;
+    [SerializeField] float ap;
+    [SerializeField] float moveSpeed;
+
+    Rigidbody rigid;
+    Transform player;
+    float hp;
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        hp = maxhp;
+    }
+    private void Update()
+    {
+        Move();
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    void Move()
+    {
+        Vector3 dir = player.position - transform.position;
+        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
+    }
+    private void HitDmage(float dmg)
+    {
+        hp -= dmg;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        HitDmage(1);
+    }
+}
